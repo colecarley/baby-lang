0452084157517bc09464f7785a08119f88eaceb7
Cole Carley <cole.carley@gmail.com>
2024-10-22 05:56:57 +0000

    add build

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main()
 {
 	std::cout << "hello world" << std::endl;
 
-	if (yyparse() == 0)
-	{
-		std::cout << "parsing completed successfully!" << std::endl;
-	}
-	else
-	{
-		std::cout << "parsing failed" << std::endl;
-	}
+	// if (yyparse() == 0)
+	// {
+	// 	std::cout << "parsing completed successfully!" << std::endl;
+	// }
+	// else
+	// {
+	// 	std::cout << "parsing failed" << std::endl;
+	// }
 
 	return 0;
 }
